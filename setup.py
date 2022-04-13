a337b39a5363f86a1ceedf33a500a5b2795569bf
Rafael <rafacaropre@gmail.com>
2022-04-13 16:53:54 +0200

    Initial commit after using the cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An exploratory DS project on images and music',
+    author='Rafael Caroprese',
+    license='MIT',
+)
